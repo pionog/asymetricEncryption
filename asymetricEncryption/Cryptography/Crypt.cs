d4a5a606e12d8071754a2d89608008015a1694d3
pionog <piotr02n@gmail.com>
2023-05-12 22:01:13 +0200

    basic implementation of encryption
    There's implemented encryption of a single file. It couldn't be tested properly so it probably doesn't work properlu. File size have to be smaller than generated key's size. Program saves public key in a txt file which will be changed in near future.

---
+++ asymetricEncryption/Cryptography/Crypt.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+
+namespace asymetricEncryption.Cryptography
+{
+    public class Crypt
+    {
+        public static byte[] encrypt(string fileName)
+        {
+            byte[] fileContent = null;
+            try
+            {
+                System.IO.FileStream fs = new System.IO.FileStream(fileName, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+                System.IO.BinaryReader binaryReader = new System.IO.BinaryReader(fs);
+                
+                long byteLength = new System.IO.FileInfo(fileName).Length;
+                fileContent = binaryReader.ReadBytes((Int32)byteLength);
+                fs.Close();
+                fs.Dispose();
+                binaryReader.Close();
+            }
+            catch (Exception ex) {
+                MessageBox.Show("There occured error with reading file. Chceck if it is a proper file.");
+            }
+            RSA rsa = RSA.Create();
+
+            var csp = new RSACryptoServiceProvider(4096);
+
+            //how to get the private key
+            var privKey = csp.ExportParameters(true);
+
+            //and the public key ...
+            var pubKey = csp.ExportParameters(false);
+            var pubKeyBytes = Convert.ToBase64String(pubKey.Modulus);
+            var publicKey = new StringBuilder();
+            publicKey.AppendLine("-----BEGIN PUBLIC KEY-----");
+            publicKey.AppendLine(pubKeyBytes);
+            publicKey.AppendLine("-----END PUBLIC KEY-----");
+            File.WriteAllText(fileName + "public_key.txt", publicKey.ToString());
+
+            byte[] encryptedBytes = null;
+
+            rsa.ImportParameters(pubKey);
+            try
+            {
+                encryptedBytes = rsa.Encrypt(fileContent, System.Security.Cryptography.RSAEncryptionPadding.OaepSHA512);
+                MessageBox.Show("Encryption has been ended successfully.");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Program was unable to successfully encrypt this file.");
+            }
+            return encryptedBytes;
+        }
+        /*public static byte[] decrypt(string fileName) {
+            byte[] fileContent = null;
+            try
+            {
+                System.IO.FileStream fs = new System.IO.FileStream(fileName, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+                System.IO.BinaryReader binaryReader = new System.IO.BinaryReader(fs);
+
+                long byteLength = new System.IO.FileInfo(fileName).Length;
+                fileContent = binaryReader.ReadBytes((Int32)byteLength);
+                fs.Close();
+                fs.Dispose();
+                binaryReader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There occured error with reading file. Chceck if it is a proper file.");
+            }
+        }*/
+    }
+}
--- asymetricEncryption/MainWindow.xaml.cs
+++ asymetricEncryption/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,52 @@ namespace asymetricEncryption
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //string path = fileTextBox.Text;
+        }
+
+        private void selectFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            bool? result = dialog.ShowDialog();
+            if (result == true)
+            {
+                string filename = dialog.FileName;
+                fileTextBox.Text = filename;
+            }
+        }
+
+        private void decryptionButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void encryptionButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void doItButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(encryptionButton.IsChecked == true)
+            {
+                try
+                {
+                    byte[] encryptedFile = Cryptography.Crypt.encrypt(fileTextBox.Text);
+                    File.WriteAllBytes(fileTextBox.Text + ".encrypted", encryptedFile);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else if(decryptionButton.IsChecked == true)
+            {
+                //Cryptography.Crypt.decrypt(fileTextBox.Text);
+            }
         }
     }
 }
