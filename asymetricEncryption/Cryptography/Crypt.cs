3f6b63e8b9a393c60208453edc737c8c30f774bc
pionog <piotr02n@gmail.com>
2023-05-13 19:40:03 +0200

    fixed some errors with decryption
    It still doesn't work as it should but program properly loads private key

--- asymetricEncryption/Cryptography/Crypt.cs
+++ asymetricEncryption/Cryptography/Crypt.cs
@@ -31,13 +31,13 @@ namespace asymetricEncryption.Cryptography
             }
             RSA rsa = RSA.Create();
 
-            var csp = new RSACryptoServiceProvider(4096);
+            var csp = new RSACryptoServiceProvider(2048);
 
             //how to get the private key
             var privKey = csp.ExportParameters(true);
             var privateKey = new StringBuilder();
-            privateKey.AppendLine("-----BEGIN RSA PUBLIC KEY-----");
-            privateKey.AppendLine(Convert.ToBase64String(rsa.ExportPkcs8PrivateKey()));
+            privateKey.AppendLine("-----BEGIN RSA PRIVATE KEY-----");
+            privateKey.AppendLine(Convert.ToBase64String(rsa.ExportRSAPrivateKey()));
             privateKey.AppendLine("-----END RSA PRIVATE KEY-----");
             File.WriteAllText(fileName + ".private_key.txt", privateKey.ToString());
 
@@ -45,7 +45,8 @@ namespace asymetricEncryption.Cryptography
             var pubKey = csp.ExportParameters(false);
             var publicKey = new StringBuilder();
             publicKey.AppendLine("-----BEGIN RSA PUBLIC KEY-----");
-            publicKey.AppendLine(Convert.ToBase64String(rsa.ExportRSAPublicKey()));
+            byte[] pk = rsa.ExportRSAPublicKey();
+            publicKey.AppendLine(Convert.ToBase64String(pk));
             publicKey.AppendLine("-----END RSA PUBLIC KEY-----");
             File.WriteAllText(fileName + ".public_key.txt", publicKey.ToString());
 
@@ -63,10 +64,10 @@ namespace asymetricEncryption.Cryptography
             }
             return encryptedBytes;
         }
-        public static byte[] decrypt(string fileName) {
+        public static byte[] decrypt(string fileName) { //filename = original file + ".encrypted"
             string originalFileName = Path.GetFileNameWithoutExtension(fileName);
             string filePath = Path.GetDirectoryName(fileName);
-            string file = Path.Combine(filePath, originalFileName);
+            string file = Path.Combine(filePath, originalFileName); //original file without ".enrypted"
             byte[] fileContent = null;
             //reading encrypted file
             try
@@ -86,17 +87,13 @@ namespace asymetricEncryption.Cryptography
                 throw;
             }
             byte[] privateKeyContent = null;
+            
             //reading private key
             try
             {
-                System.IO.FileStream fs = new System.IO.FileStream(file + ".private_key.txt", System.IO.FileMode.Open, System.IO.FileAccess.Read);
-                System.IO.BinaryReader binaryReader = new System.IO.BinaryReader(fs);
-
-                long byteLength = new System.IO.FileInfo(file + ".private_key.txt").Length;
-                privateKeyContent = binaryReader.ReadBytes((Int32)byteLength);
-                fs.Close();
-                fs.Dispose();
-                binaryReader.Close();
+                string[] strings = File.ReadAllLines(file + ".private_key.txt");
+                string text = strings[1]; //[0] - header, [1] - private key [2] - footer
+                privateKeyContent = Convert.FromBase64String(text);
             }
             catch (Exception ex)
             {
@@ -104,11 +101,12 @@ namespace asymetricEncryption.Cryptography
                 throw;
             }
             RSA rsa = RSA.Create();
+            var csp = new RSACryptoServiceProvider(2048);
 
-            int numberOfBytes;
+            //int numberOfBytes;
             try
             {
-                rsa.ImportPkcs8PrivateKey(privateKeyContent, out numberOfBytes);
+                rsa.ImportRSAPrivateKey(privateKeyContent, out _);
             }
             catch (Exception ex) {
                 MessageBox.Show("There occured an error with reading the private key. Chceck if there is a proper file with private key in the same directory.");
--- asymetricEncryption/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ asymetricEncryption/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -101,7 +101,7 @@ namespace asymetricEncryption {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/asymetricEncryption;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/asymetricEncryption;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
