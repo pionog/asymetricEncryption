5dfa94a28e91fb5924ab53dc78b2b4eebb975e3b
pionog <piotr02n@gmail.com>
2023-05-26 16:49:22 +0200

    Parting files into the blocks
    Program is now able to encrypt and decrypt bigger files.

--- asymetricEncryption/Cryptography/Crypt.cs
+++ asymetricEncryption/Cryptography/Crypt.cs
@@ -4,17 +4,22 @@ using System.Security.Cryptography;
 using System.IO;
 using System.Windows;
 using System.Numerics;
+using System.Reflection.PortableExecutable;
+using System.Buffers.Text;
 
 namespace asymetricEncryption.Cryptography
 {
     public class Crypt
     {
+        static int byteSize = 256-11; //256 size of byte - 11 size of minimal padding for pkcs
         public static void encrypt(string fileName)
         {
             if(!File.Exists(fileName))
             {
                 throw new FileNotFoundException("Chceck if it is a proper file.");
             }
+            long howManyBlocks;
+            byte[][] blocks;
             byte[]? fileContent = null;
             try
             {
@@ -22,7 +27,20 @@ namespace asymetricEncryption.Cryptography
                 System.IO.BinaryReader binaryReader = new System.IO.BinaryReader(fs, Encoding.UTF8, false);
 
                 long byteLength = new System.IO.FileInfo(fileName).Length;
-                fileContent = binaryReader.ReadBytes((Int32)byteLength);
+                howManyBlocks = byteLength / byteSize + 1;
+                blocks = new byte[howManyBlocks][];
+                for (int i = 0; i < howManyBlocks; i++) {
+                    if (byteLength >= byteSize)
+                    {
+                        blocks[i] = binaryReader.ReadBytes(byteSize);
+                    }
+                    else
+                    {
+                        blocks[i] = binaryReader.ReadBytes((Int32)byteLength);
+                    }
+                    byteLength -= byteSize;
+                }
+                //fileContent = binaryReader.ReadBytes((Int32)byteLength);
                 fs.Close();
                 fs.Dispose();
                 binaryReader.Close();
@@ -48,19 +66,35 @@ namespace asymetricEncryption.Cryptography
             publicKey.AppendLine("-----END RSA PUBLIC KEY-----");
             File.WriteAllText(fileName + ".public_key.txt", publicKey.ToString());
 
-            byte[] encryptedBytes;
+            byte[][] encryptedBlocks = new byte[howManyBlocks][];
+            //byte[] encryptedBytes;
             try
             {
-                encryptedBytes = csp.Encrypt(fileContent, System.Security.Cryptography.RSAEncryptionPadding.Pkcs1);
+                for (int i = 0; i < howManyBlocks; i++) {
+                    encryptedBlocks[i] = csp.Encrypt(blocks[i], System.Security.Cryptography.RSAEncryptionPadding.Pkcs1);
+                }
+                //encryptedBytes = csp.Encrypt(fileContent, System.Security.Cryptography.RSAEncryptionPadding.Pkcs1);
             }
             catch
             {
                 throw new CryptographicException("Program was unable to successfully encrypt this file.");
             }
-            string text = Convert.ToBase64String(encryptedBytes);
+            string[] blocksText = new string[howManyBlocks];
+            for (int i = 0; i < howManyBlocks; i++)
+            {
+                blocksText[i] = Convert.ToBase64String(encryptedBlocks[i]);
+            }
+            //string text = Convert.ToBase64String(encryptedBytes);
             try
             {
-                File.WriteAllText(fileName + ".encrypted", text);
+                using (StreamWriter writer = File.CreateText(fileName + ".encrypted"))
+                {
+                    for (int i = 0; i < howManyBlocks; i++)
+                    {
+                        writer.Write(blocksText[i]);
+                    }
+                }
+                //File.WriteAllText(fileName + ".encrypted", text);
             }
             catch {
                 throw new ArgumentException("Could not save an encrypted file");
@@ -74,16 +108,38 @@ namespace asymetricEncryption.Cryptography
             string originalFileName = Path.GetFileNameWithoutExtension(fileName);
             string filePath = Path.GetDirectoryName(fileName);
             string file = Path.Combine(filePath, originalFileName); //original file without ".enrypted"
-            byte[] fileContent = null;
+            //byte[] fileContent = null;
+            byte[] fileContent = new byte[1];
+            string textFile;
+            long howManyBlocks;
             try
             {
-               string base64 = File.ReadAllText(fileName);
-                fileContent = System.Convert.FromBase64String(base64);
+                using (StreamReader sr = new StreamReader(fileName, Encoding.UTF8, false))
+                {
+                    textFile = sr.ReadToEnd();
+                }
+
+                //string base64 = File.ReadAllText(fileName);
+                //fileContent = System.Convert.FromBase64String(base64);
+
             }
+
             catch
             {
                 throw new ArgumentException("Program was unable to successfully encrypt this file.");
             }
+            string[] lines = textFile.Split("==");
+            howManyBlocks = lines.Length - 1;
+            for (int i = 0; i < howManyBlocks; i++)
+            {
+                lines[i] += "=="; // adding "==" characters to restore them from Split() method
+            }
+            byte[][] fileBlocks = new byte[howManyBlocks][];
+            for (int i = 0; i < howManyBlocks; i++)
+            {
+                fileBlocks[i] = System.Convert.FromBase64String(lines[i]);
+            }
+
             byte[] privateKeyContent;
             
             //reading private key
@@ -105,10 +161,15 @@ namespace asymetricEncryption.Cryptography
             }
             var csp = new RSACryptoServiceProvider(2048);
             csp.ImportRSAPrivateKey(privateKeyContent, out _);
-            byte[]? decryptedBytes;
+            byte[][] decryptedBlocks = new byte[howManyBlocks][];
+            //byte[]? decryptedBytes;
+            byte[]? decryptedBytes = new byte[1];
             try
             {
-                decryptedBytes = csp.Decrypt(fileContent, System.Security.Cryptography.RSAEncryptionPadding.Pkcs1);
+                for (int i = 0; i < howManyBlocks; i++) {
+                    decryptedBlocks[i] = csp.Decrypt(fileBlocks[i], System.Security.Cryptography.RSAEncryptionPadding.Pkcs1);
+                }
+                //decryptedBytes = csp.Decrypt(fileContent, System.Security.Cryptography.RSAEncryptionPadding.Pkcs1);
             }
             catch
             {
@@ -121,7 +182,26 @@ namespace asymetricEncryption.Cryptography
             string fileResult = fileWE + "_result" + extension;
             string path = System.IO.Path.GetDirectoryName(fileName);
             string fileNameDecrypted = System.IO.Path.Combine(path, fileResult);
-            File.WriteAllBytes(fileNameDecrypted, decryptedBytes);
+            try
+            {
+                System.IO.FileStream fs = new System.IO.FileStream(fileNameDecrypted, System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.Write);
+                System.IO.BinaryWriter binaryWriter = new System.IO.BinaryWriter(fs, Encoding.UTF8, false);
+
+                for (int i = 0; i < howManyBlocks; i++)
+                {
+                     binaryWriter.Write(decryptedBlocks[i]);
+                }
+                //fileContent = binaryReader.ReadBytes((Int32)byteLength);
+                fs.Close();
+                fs.Dispose();
+                binaryWriter.Close();
+            }
+            catch
+            {
+                throw new ArgumentException("There occured an error while reading file.");
+            }
+
+            //File.WriteAllBytes(fileNameDecrypted, decryptedBytes);
             return;
         }
         public static void modify(string fileName, int whichByte) {
